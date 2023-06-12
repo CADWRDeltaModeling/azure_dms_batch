44ae3e8af782e23d4e31244f416a4a7b25a3a7de
Nicky Sandhu <psandhu@water.ca.gov>
2023-06-12 08:20:12 -0700

    more fixes on scripts

--- schism_scripts/batch/make_root_ssh_passwordless.sh
+++ schism_scripts/batch/make_root_ssh_passwordless.sh
@@ -1,5 +1,6 @@
 ## Enable RDMA communication for root user
-unalias cp
+echo "Enabling root ssh passwordless"
+[[ $(type -t cp) == "alias" ]] && unalias cp
 cp -rv /home/_azbatch/.ssh /root/.
 sed -i 's/home\/_azbatch/root/g' /root/.ssh/config
 chmod 700 /root/.ssh
@@ -7,3 +8,4 @@ chmod 644 /root/.ssh/config
 chmod 644 /root/.ssh/authorized_keys
 sed -i 's/PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl reload sshd
+echo "Done enabling root ssh passwordless"
--- schism_scripts/batch/pool_startup.sh
+++ schism_scripts/batch/pool_startup.sh
@@ -1,5 +1,6 @@
 # This script is run on the pool nodes when they are created.
 # Especially needed for custom images, where installs are permanent but these settings are not.
+cd /opt/schism_scripts/batch
 source ./enable_sudo_for_batch.sh
 source ./make_root_ssh_passwordless.sh
 source ./appinsights_start.sh
