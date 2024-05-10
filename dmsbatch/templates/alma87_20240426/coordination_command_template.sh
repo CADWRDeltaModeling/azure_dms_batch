3271e2299f36144d1fe7ee0b3565321ec321b54d
Nicky Sandhu <psandhu@water.ca.gov>
2024-05-10 16:06:06 -0700

    adding targz conda packed env into zip for linux compat

--- dmsbatch/templates/alma87_20240426/coordination_command_template.sh
+++ dmsbatch/templates/alma87_20240426/coordination_command_template.sh
@@ -1,5 +1,8 @@
 echo "running nfs install script";
 export SCHISM_SCRIPTS_HOME=$AZ_BATCH_APP_PACKAGE_batch_setup_alma8_7;
+pushd $AZ_BATCH_APP_PACKAGE_schimpy_with_deps_rhel8_7; 
+tar xvzf schism.tar.gz && rm -f schism.tar.gz;
+popd;
 sudo -E $SCHISM_SCRIPTS_HOME/batch/nfs_start.sh;
 echo "linking to nfs /shared/data as simulations and changing directory to simulations";
 ln -s /shared/data $AZ_BATCH_TASK_WORKING_DIR/simulations;
