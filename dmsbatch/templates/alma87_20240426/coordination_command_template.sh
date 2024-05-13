c907a1a9c8a5a80b10352d8ee9d0bcfe590bd12b
dwr-psandhu <psandhu@water.ca.gov>
2024-05-13 13:30:11 -0700

    loading hcoll using hpcx

--- dmsbatch/templates/alma87_20240426/coordination_command_template.sh
+++ dmsbatch/templates/alma87_20240426/coordination_command_template.sh
@@ -1,7 +1,7 @@
 echo "running nfs install script";
 export SCHISM_SCRIPTS_HOME=$AZ_BATCH_APP_PACKAGE_batch_setup_alma8_7;
 pushd $AZ_BATCH_APP_PACKAGE_schimpy_with_deps_rhel8_7; 
-tar xvzf schism.tar.gz && rm -f schism.tar.gz;
+tar -xzf schism.tar.gz && rm -f schism.tar.gz;
 popd;
 sudo -E $SCHISM_SCRIPTS_HOME/batch/nfs_start.sh;
 echo "linking to nfs /shared/data as simulations and changing directory to simulations";
