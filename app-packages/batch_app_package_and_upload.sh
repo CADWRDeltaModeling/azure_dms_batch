d5b5c3735a9a969463ed063cd1b4dea6cfbfd778
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2025-04-24 13:18:12 -0700

    add suxarray_with_deps package 20250424 for specific suxarray fxns

--- app-packages/batch_app_package_and_upload.sh
+++ app-packages/batch_app_package_and_upload.sh
@@ -117,6 +117,33 @@ package_and_upload_schimpy(){
     popd
 }
 
+package_and_upload_suxarray_with_deps(){
+    batch_name=$1
+    resource_group_name=$2
+    app_name="suxarray_with_deps"
+    # todays date in 2024.06.11 format
+    today=$(date +"%Y.%m.%d")
+    version=${today}
+    rm -rf /tmp/suxarray__with_deps_${version}
+    mkdir -p /tmp/suxarray__with_deps_${version}
+    pushd /tmp/suxarray_with_deps_${version}
+    conda env remove -n suxarray_${version} -y || true
+    conda create -n suxarray_${version} -y -c conda-forge python=3.11 pandas xarray dask netcdf4 h5netcdf numba scipy scikit-learn matplotlib pyarrow requests spatialpandas cartopy datashader antimeridian shapely geoviews pyogrio uxarray=2024.07.1
+    conda activate suxarray
+    pip install --no-deps git+https://github.com/cadwrdeltamodeling/suxarray.git@v2024.09.0
+    conda activate pack
+    conda pack -n suxarray_${version} -o suxarray.tar.gz
+    zip -r suxarray_${version}.zip suxarray.tar.gz
+    conda deactivate
+    conda env remove -n suxarray_${version} -y
+    package_file="suxarray_${version}.zip"
+
+    module load azure_cli
+    az batch application package create --application-name "${app_name}" --name ${batch_name} --package-file "${package_file}" -g ${resource_group_name} --version-name "${version}"
+    az batch application set --application-name "${app_name}" --default-version "${version}" --name ${batch_name} --resource-group ${resource_group_name}
+    popd
+}
+
 package_and_upload_suxarray(){
     batch_name=$1
     resource_group_name=$2
@@ -190,3 +217,4 @@ package_and_upload_pydelmod(){
 #package_and_upload_schimpy dwrbdodspbatch dwrbdo_dsp
 #package_and_upload_pydelmod dwrmodelingbatchaccount azure_model_batch
 #package_and_upload_suxarray dwrbdodspbatch dwrbdo_dsp
+package_and_upload_suxarray_with_deps schismbatch dwrbdo_schism_rg
