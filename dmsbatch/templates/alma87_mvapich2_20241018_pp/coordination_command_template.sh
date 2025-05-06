f3b3680912f5c202ab799606c7c3f8748f57f3a0
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2025-05-06 12:12:48 -0700

    add suxarray to pp template

--- dmsbatch/templates/alma87_mvapich2_20241018_pp/coordination_command_template.sh
+++ dmsbatch/templates/alma87_mvapich2_20241018_pp/coordination_command_template.sh
@@ -1,1 +1,6 @@
+echo "running nfs install script";
+tar -xzf schimpy.tar.gz && rm -f schimpy.tar.gz;
+pushd $AZ_BATCH_APP_PACKAGE_suxarray_with_deps; 
+tar -xzf suxarray.tar.gz && rm -f suxarray.tar.gz;
+popd;
 echo Coordination Task Done
